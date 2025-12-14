1552aa53b09b279a35758c2fb13f14da8576f69f
praveenkrwebdev <praveenkr.webdev@gmail.com>
2025-12-14 16:42:44 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -4,10 +4,12 @@ import { getAuth } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAmmMXeR4phiAxFLeI_tAA_CftH11Zm9rA",
+  authDomain: "stammering-words.firebaseapp.com",
+  projectId: "stammering-words",
+  storageBucket: "stammering-words.firebasestorage.app",
+  messagingSenderId: "921650228054",
+  appId: "1:921650228054:web:0f7c8fd85e7dc11f31c4f7"
 };
 
 export const app = initializeApp(firebaseConfig);
